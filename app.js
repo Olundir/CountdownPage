43163e22e4b3b019ca7fd8de2a15d9939d79c6d4
Olundir <krzysztof.jezak.contact@gmail.com>
2021-07-27 18:21:46 +0200

    fixed the overlap

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const placeholder = document.querySelectorAll(".placeholder");
 
 const changeSeconds = async () => {
   topAnim[3].classList.add("topAnimation");
-  bottomAnim[3].classList.add("topNextAnimation");
+  bottomAnim[3].classList.add("topNextAnimation", "visibility");
+  bottomC[3].classList.add("visibility");
   setTimeout(() => {
     placeholder[3].classList.add("putOnTop");
     placeholder[0].classList.add("putOnTop");
@@ -18,4 +19,13 @@ const changeValSeconds = () => {
   return parseInt((topAnim[3].value = topAnim[3].value - 1));
 };
 
-changeSeconds();
+// function to manage the time change
+const changeTime = async () => {
+  changeSeconds();
+};
+
+// this sets off the timer
+setInterval(function () {
+  changeTime();
+  console.log("I changed");
+}, 1000);
