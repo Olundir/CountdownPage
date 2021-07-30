6aff1af1437cf046ebb56afd5e27f14738c83363
Olundir <krzysztof.jezak.contact@gmail.com>
2021-07-30 09:58:36 +0200

    logic behind the clock is DONE

--- app.js
+++ app.js
@@ -3,36 +3,59 @@ const bottomAnim = document.querySelectorAll(".topNext");
 const topC = document.querySelectorAll(".bottom");
 const bottomC = document.querySelectorAll(".bottomNext");
 const placeholder = document.querySelectorAll(".placeholder");
+let minTim = parseInt(topC[3].innerText) + parseInt(topC[2].innerText * 60) - 1;
+let hTim = parseInt(topC[3].innerText) + parseInt(topC[2].innerText * 60) + parseInt(topC[1].innerText * 3600) - 1;
 
-const flipCard = async (index) => {
-  placeholder[index].classList.remove("putOnTop");
+const flipCard = async (index, number) => {
   topAnim[index].classList.add("topAnimation");
   bottomAnim[index].classList.add("topNextAnimation", "visibility");
   bottomC[index].classList.add("visibility");
   setTimeout(() => {
-    placeholder[index].classList.add("putOnTop");
-    placeholder[index].innerText = placeholder[index].innerText - 1;
-    changeValue(index);
+    changeValueMinSec(index, number);
     reset(index);
   }, 950);
 };
 
-const changeValue = async (index) => {
-  topAnim[index].innerText = parseInt(topAnim[index].innerText - 1);
-  topC[index].innerText = parseInt(topC[index].innerText - 1);
-  bottomAnim[index].innerText = parseInt(bottomAnim[index].innerText - 1);
-  bottomC[index].innerText = parseInt(bottomC[index].innerText - 1);
+const changeValueMinSec = async (index, number) => {
+  if (topC[index].innerText === "0") {
+    topAnim[index].innerText = number; // number
+    topC[index].innerText = number;
+    bottomAnim[index].innerText = number - 1; // number - 1
+    bottomC[index].innerText = number - 1;
+  } else if (topC[index].innerText === "1") {
+    topAnim[index].innerText = 0;
+    topC[index].innerText = 0;
+    bottomAnim[index].innerText = number; // number
+    bottomC[index].innerText = number;
+  } else {
+    topAnim[index].innerText = parseInt(topAnim[index].innerText - 1);
+    topC[index].innerText = parseInt(topC[index].innerText - 1);
+    bottomAnim[index].innerText = parseInt(bottomAnim[index].innerText - 1);
+    bottomC[index].innerText = parseInt(bottomC[index].innerText - 1);
+  }
 };
 
 const reset = async (index) => {
   topAnim[index].classList.remove("topAnimation");
   bottomAnim[index].classList.remove("topNextAnimation", "visibility");
   bottomC[index].classList.remove("visibility");
-  placeholder[index].classList.remove("putOnTop");
 };
 
 const initialize = async () => {
-  flipCard(3);
+  flipCard(3, 59);
+  if (topC[3].innerText === "1") {
+    flipCard(2, 59);
+  }
+  if (minTim === 0) {
+    flipCard(1, 23);
+    minTim = 3600;
+  }
+  if (hTim === 0) {
+    flipCard(0, 8);
+    hTim = 86400;
+  }
+  minTim--;
+  hTim--;
 };
 
 // this sets off the timer
@@ -41,5 +64,3 @@ let time = setInterval(initialize, 1000);
 function stopTime() {
   clearInterval(time);
 }
-
-// ------------------------------------------- //
